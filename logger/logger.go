99f95f53b5f9ef7fd71118d17a3bb7cc84bb211c
Scott Ferguson <scottwferg@gmail.com>
2013-10-29 19:24:21 -0500

    Add logger package

---
+++ logger/logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"strings"
+)
+
+type DBLogger struct {
+	database   *mgo.Database
+	collection string
+}
+
+type Entry struct {
+	Message string `bson:"_id"`
+}
+
+func New(c string, db *mgo.Database) *DBLogger {
+	return &DBLogger{
+		database:   db,
+		collection: c,
+	}
+}
+
+func (l *DBLogger) Write(data []byte) (int, error) {
+	err := l.database.C(l.collection).Insert(Entry{
+		Message: strings.TrimSpace(string(data)),
+	})
+
+	return len(data), err
+}
+
+func Tail(out io.Writer, log string, db *mgo.Database) {
+	var entry Entry
+    // Find the last entry in the tailable collection, then 
+    // use that to determine where to begin the cursor
+	db.C(log).Find(nil).Sort("-$natural").Limit(1).One(&entry)
+
+	query := func(id string) *mgo.Query {
+		return db.C(log).Find(bson.M{
+			"_id": bson.M{
+				"$gt": id,
+			},
+		})
+	}
+
+	iter := query(entry.Message).Sort("$natural").Tail(-1)
+	for {
+		for iter.Next(&entry) {
+			fmt.Fprintf(out, entry.Message)
+
+			if err := iter.Close(); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+            iter = query(entry.Message).Sort("$natural").Tail(-1)
+		}
+	}
+}
